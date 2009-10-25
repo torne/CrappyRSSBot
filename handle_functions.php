7bc1206ff679419208f43515f2bd3e89c083c989
Gabriel Baker <gabriel@403.be>
2009-10-25 18:04:04 +0000

    lol wot a commit!

--- handle_functions.php
+++ handle_functions.php
@@ -39,7 +39,7 @@ class handle_functions
 				$bot->quit( $quitmessage );
 				break;
 			case "lemons":
-				$bot->sendMsg($returnDest, "what about a coconut?" );
+				$bot->sendMsg($returnDest, "don't forget the limes!" );
 				break;
 			case "reload":
 				$reload = new classReloader();
